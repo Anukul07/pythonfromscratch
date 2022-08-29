ff22eb3d86f9d6eaba8961126c18b03ec7358a88
Anukul Nepal <anukulnepal2014@gmail.com>
2022-08-29 11:15:28 +0545

    methods

---
+++ scratch.py
@@ -0,0 +1,42 @@
+""" name = "Eric"
+print(f"Hello {name.lower()}, this is your first!")
+print(name.lower())
+print(name.upper())
+print(name.title())
+ """
+
+
+# author=" Nikola\t\nTesla "
+""" message = f"{author.title()} once said, 'Be alone that is when ideas...'"
+ print(message)
+print(author.lstrip())
+print(author.rstrip())
+print(author.strip())"""
+
+
+""" names= ['Ram', 'Sita', 'Hira', 'Gita']
+print(f"I love {names[1].title()}") """
+
+# motorcycles = ['Honda', 'Suzuki', 'BMW']
+# print(motorcycles)
+# motorcycles[0]= 'Ducati'
+# motorcycles = []
+# motorcycles.append('Ducati')
+# motorcycles.append('Suzuki')
+# motorcycles = ['Honda', 'Suzuki', 'BMW']
+# del motorcycles[0]
+# print(motorcycles) 
+
+# Removing an item using the pop() method:
+""" motorcycles = ['Honda', 'Suzuki', 'BMW']
+print(motorcycles)
+popped_motorcycles= motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycles)
+ """
+
+#  inserting Elements into a list :
+""" motorcycles = ['Honda', 'Suzuki', 'BMW']
+motorcycles.insert(0, 'Ducati')
+print(motorcycles)
+ """
